901c67854c81e0b96bcb8350d5f9e81e39b40068
MiltonTomasino <jonathantomasino13@gmail.com>
2025-10-21 22:58:38 -0700

    Add Direct Messaging Support in Database

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
