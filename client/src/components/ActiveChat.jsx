0c29fee8a292d5d13fdaf684b2ca44ef38b4dd7e
MiltonTomasino <jonathantomasino13@gmail.com>
2025-10-28 14:50:36 -0700

    Add Socket.io Functionality to the Messages
    There is now real time messageing between 2 users.

--- client/src/components/ActiveChat.jsx
+++ client/src/components/ActiveChat.jsx
@@ -1,9 +1,55 @@
+import { useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { UserContext } from "../context/UserContext";
+
+let socket;
+
 function ActiveChat({ setActiveChat, activeChat }) {
+
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
+    const context = useContext(UserContext)
+
+    useEffect(() => {
+        socket = io("http://localhost:3000");
+
+        socket.emit("joinChat", activeChat.id);
+
+        socket.on("newMessage", (msg) => {
+            setMessages(prev => [...prev, msg])
+        })
+
+        return () => {
+            socket.disconnect();
+        };
+    }, [activeChat.id])
+
+    function handleSendMessage() {
+        socket.emit("sendMessage", {
+            chatId: activeChat.id,
+            message: newMessage,
+            userId: context.user.id
+        });
+        setNewMessage("");
+    }
+
     return (
         <>
             <button onClick={() => setActiveChat(null)}>back</button>
             <h1>Active chat</h1>
             <p>{activeChat.id}</p>
+            <div className="messages-window">
+                {messages.map((msg, idx) => {
+                    return <p key={idx}>{msg.text}</p>
+                })}
+            </div>
+            <div className="message-input">
+                <input
+                type="text"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}/>
+                <button onClick={handleSendMessage}>send</button>
+            </div>
         </>
     )
 };
--- server/app.js
+++ server/app.js
@@ -5,10 +5,20 @@ const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 const prisma = require("./db/prisma");
 const PORT = process.env.PORT || 3000;
 const userRouter = require("./routes/user");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
 require("dotenv").config();
 require("./passport")
 
 const app = express();
+const server = createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -37,4 +47,25 @@ app.use(passport.session());
 
 app.use("/api", userRouter);
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+io.on("connection", (socket) => {
+    console.log("A user has connected");
+
+    socket.on("joinChat", (chatId) => {
+        socket.join(chatId);
+        console.log(`User joined chat ${chatId}`);
+        
+    });
+
+    socket.on("sendMessage", ({ chatId, message, userId }) => {
+        const msg = { text: message, userId, createdAt: new Date() };
+
+        io.to(chatId).emit("newMessage", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+        
+    })
+})
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
